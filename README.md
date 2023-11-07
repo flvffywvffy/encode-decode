1c92e73acffc867cae73d0adaf80968fc5704228
flvffywvffy <flvffywvffy@gmail.com>
2023-11-07 12:03:04 +0000

    Added url encoding option, updated readme.

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 Encode & decode text to and from base64.
 
 ## Installation
-My extention is currently being reviewed, so I'll upload a link to the chrome web store listing after that.
+**These buttons may not be working yet as the extention is being reviewd by Mozilla and Google.**
+[![](https://storage.googleapis.com/web-dev-uploads/image/WlD8wC6g8khYWPJUsQceQkhXSlv1/UV4C4ybeBTsZt43U4xis.png)](https://chromewebstore.google.com/detail/fbhlbfckcmgpkcbogmmaomgebfgmidon)
+[![](https://extensionworkshop.com/assets/img/documentation/publish/get-the-addon-178x60px.dad84b42.png)](https://addons.mozilla.org/en-GB/firefox/addon/encodedecode/)
 
 ## Screenshots
 ![](https://github.com/flvffywvffy/encode-decode/blob/main/screenshots/01.png)
--- js/convert.js
+++ js/convert.js
@@ -1,14 +1,27 @@
-const optionsDropdown = document.getElementById("encode-type")
+const encoderDropdown = document.getElementById("encoder");
+const functionDropdown = document.getElementById("encoderFunction");
 const textArea = document.getElementById("textarea");
 const convertBtn = document.getElementById("convert");
 
 convertBtn.addEventListener("click", () => {
-    const currentOption = optionsDropdown.value;
+  const encoderType = encoderDropdown.value;
+  if (encoderType == "base64") {
+    const currentOption = functionDropdown.value;
     if (currentOption == "encode") {
-        const newVal = btoa(textArea.value);
-        textArea.value = newVal;
+      const newVal = btoa(textArea.value);
+      textArea.value = newVal;
     } else if (currentOption == "decode") {
-        const newVal = atob(textArea.value);
-        textArea.value = newVal;
+      const newVal = atob(textArea.value);
+      textArea.value = newVal;
     }
-})
+  } else if (encoderType == "url") {
+    const currentOption = functionDropdown.value;
+    if (currentOption == "encode") {
+      const newVal = encodeURI(textArea.value);
+      textArea.value = newVal;
+    } else if (currentOption == "decode") {
+      const newVal = decodeURI(textArea.value);
+      textArea.value = newVal;
+    }
+  }
+});
